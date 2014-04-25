855d26e6bf968d92afc87ad57732bf4c616b1fca
Prashant Gaurav <prashant@groupon.com>
2014-04-25 13:55:40 -0700

    minor fix

--- sql/codegen.py
+++ sql/codegen.py
@@ -7,9 +7,9 @@ ff = ['bought_category',\
 'bought_company_brand_category']
 
 for f in ff:
-    print f
-    print "%s_q," %(f)
-    print "%s_a," %(f)
+    print '%s,' %(f)
+    print '%s_q,' %(f)
+    print '%s_a,' %(f)
     print 'bucketize(%s,0,1,2,5,10,20) b%s,' % (f,f)
     print 'bucketize(%s_q,0,1,2,5,10,20) b%s_q,' % (f,f)
     print 'bucketize(%s_a,0,15,30,50,100,200) b%s_a,' % (f,f)
